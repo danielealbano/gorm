3269ae55748acc8739759ac7f469007ed014185d
Daniele Salvatore Albano <d.albano@gmail.com>
2024-07-17 15:18:32 +0200

    Use only the trace function, do not print the SQL as-is

--- migrator/migrator.go
+++ migrator/migrator.go
@@ -48,7 +48,6 @@ type printSQLLogger struct {
 
 func (l *printSQLLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
 	sql, _ := fc()
-	fmt.Println(sql + ";")
 	l.Interface.Trace(ctx, begin, fc, err)
 }
 
